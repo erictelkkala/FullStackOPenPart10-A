3faf274c22d75ba2a63c531627884c88f4a7891c
Eric Telkkälä <52538447+erictelkkala@users.noreply.github.com>
2022-10-04 19:07:44 +0300

    Task 10.6

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -8,14 +8,14 @@ const styles = StyleSheet.create({
 		paddingTop: Constants.statusBarHeight * 1.5,
 		padding: Constants.statusBarHeight,
 		backgroundColor: '#24292e',
-		alignContent: "center"
 	}
 });
 
 const AppBar = () => {
 	return (
 		<View style={styles.container}>
-			<AppBarTab text={'Repositories'} />
+			<AppBarTab text={'Repositories'} navigateTo={"/"} />
+			<AppBarTab text={"Sign In"} navigateTo={"/signin"} />
 		</View>
 	);
 };
--- src/components/AppBarTab.jsx
+++ src/components/AppBarTab.jsx
@@ -1,19 +1,23 @@
 import {Pressable, Text, StyleSheet} from "react-native";
+import {Link} from "react-router-native";
 
 const styles = StyleSheet.create({
 	text: {
 		fontSize: 15,
 		fontWeight: 'bold',
-		color: 'white'
+		color: 'white',
+		paddingHorizontal: 10
 	}
 });
 
 const AppBarTab = (props) => {
 	return (
 		<Pressable hitSlop={3}>
-			<Text style={styles.text}>
-				{props.text}
-			</Text>
+			<Link to={props.navigateTo}>
+				<Text style={styles.text}>
+					{props.text}
+				</Text>
+			</Link>
 		</Pressable>
 	);
 };
